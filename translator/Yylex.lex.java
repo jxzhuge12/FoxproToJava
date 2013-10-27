97c31e88b7a1559e644f50548647601f77bb7da7
jxzhuge12 <jxzhuge12@gmail.com>
2013-10-27 13:56:34 +0800

    push

---
+++ translator/Yylex.lex.java
@@ -0,0 +1,456 @@
+package translator;
+import java_cup.runtime.Symbol;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+
+class Yylex implements java_cup.runtime.Scanner {
+	private final int YY_BUFFER_SIZE = 512;
+	private final int YY_F = -1;
+	private final int YY_NO_STATE = -1;
+	private final int YY_NOT_ACCEPT = 0;
+	private final int YY_START = 1;
+	private final int YY_END = 2;
+	private final int YY_NO_ANCHOR = 4;
+	private final int YY_BOL = 128;
+	private final int YY_EOF = 129;
+
+	public static void main(String args[]) throws Exception {
+		InputStream is = new FileInputStream(args[0]);
+		Yylex lexer = new Yylex(is);
+		Symbol token = null;
+		do {
+			token = lexer.next_token();
+			System.out.println(token == null ? "EOF" : token.toString());
+		} while (token != null);
+	}
+	private int countLines(String str){
+		int count = 0;
+		for(int i = 0; i < str.length(); ++i){
+			if(str.charAt(i) == '\n'){
+				++count;
+			}
+		}
+		return count;
+	}
+	private java.io.BufferedReader yy_reader;
+	private int yy_buffer_index;
+	private int yy_buffer_read;
+	private int yy_buffer_start;
+	private int yy_buffer_end;
+	private char yy_buffer[];
+	private int yyline;
+	private boolean yy_at_bol;
+	private int yy_lexical_state;
+
+	Yylex (java.io.Reader reader) {
+		this ();
+		if (null == reader) {
+			throw (new Error("Error: Bad input stream initializer."));
+		}
+		yy_reader = new java.io.BufferedReader(reader);
+	}
+
+	Yylex (java.io.InputStream instream) {
+		this ();
+		if (null == instream) {
+			throw (new Error("Error: Bad input stream initializer."));
+		}
+		yy_reader = new java.io.BufferedReader(new java.io.InputStreamReader(instream));
+	}
+
+	private Yylex () {
+		yy_buffer = new char[YY_BUFFER_SIZE];
+		yy_buffer_read = 0;
+		yy_buffer_index = 0;
+		yy_buffer_start = 0;
+		yy_buffer_end = 0;
+		yyline = 0;
+		yy_at_bol = true;
+		yy_lexical_state = YYINITIAL;
+	}
+
+	private boolean yy_eof_done = false;
+	private final int YYINITIAL = 0;
+	private final int yy_state_dtrans[] = {
+		0
+	};
+	private void yybegin (int state) {
+		yy_lexical_state = state;
+	}
+	private int yy_advance ()
+		throws java.io.IOException {
+		int next_read;
+		int i;
+		int j;
+
+		if (yy_buffer_index < yy_buffer_read) {
+			return yy_buffer[yy_buffer_index++];
+		}
+
+		if (0 != yy_buffer_start) {
+			i = yy_buffer_start;
+			j = 0;
+			while (i < yy_buffer_read) {
+				yy_buffer[j] = yy_buffer[i];
+				++i;
+				++j;
+			}
+			yy_buffer_end = yy_buffer_end - yy_buffer_start;
+			yy_buffer_start = 0;
+			yy_buffer_read = j;
+			yy_buffer_index = j;
+			next_read = yy_reader.read(yy_buffer,
+					yy_buffer_read,
+					yy_buffer.length - yy_buffer_read);
+			if (-1 == next_read) {
+				return YY_EOF;
+			}
+			yy_buffer_read = yy_buffer_read + next_read;
+		}
+
+		while (yy_buffer_index >= yy_buffer_read) {
+			if (yy_buffer_index >= yy_buffer.length) {
+				yy_buffer = yy_double(yy_buffer);
+			}
+			next_read = yy_reader.read(yy_buffer,
+					yy_buffer_read,
+					yy_buffer.length - yy_buffer_read);
+			if (-1 == next_read) {
+				return YY_EOF;
+			}
+			yy_buffer_read = yy_buffer_read + next_read;
+		}
+		return yy_buffer[yy_buffer_index++];
+	}
+	private void yy_move_end () {
+		if (yy_buffer_end > yy_buffer_start &&
+		    '\n' == yy_buffer[yy_buffer_end-1])
+			yy_buffer_end--;
+		if (yy_buffer_end > yy_buffer_start &&
+		    '\r' == yy_buffer[yy_buffer_end-1])
+			yy_buffer_end--;
+	}
+	private boolean yy_last_was_cr=false;
+	private void yy_mark_start () {
+		int i;
+		for (i = yy_buffer_start; i < yy_buffer_index; ++i) {
+			if ('\n' == yy_buffer[i] && !yy_last_was_cr) {
+				++yyline;
+			}
+			if ('\r' == yy_buffer[i]) {
+				++yyline;
+				yy_last_was_cr=true;
+			} else yy_last_was_cr=false;
+		}
+		yy_buffer_start = yy_buffer_index;
+	}
+	private void yy_mark_end () {
+		yy_buffer_end = yy_buffer_index;
+	}
+	private void yy_to_mark () {
+		yy_buffer_index = yy_buffer_end;
+		yy_at_bol = (yy_buffer_end > yy_buffer_start) &&
+		            ('\r' == yy_buffer[yy_buffer_end-1] ||
+		             '\n' == yy_buffer[yy_buffer_end-1] ||
+		             2028/*LS*/ == yy_buffer[yy_buffer_end-1] ||
+		             2029/*PS*/ == yy_buffer[yy_buffer_end-1]);
+	}
+	private java.lang.String yytext () {
+		return (new java.lang.String(yy_buffer,
+			yy_buffer_start,
+			yy_buffer_end - yy_buffer_start));
+	}
+	private int yylength () {
+		return yy_buffer_end - yy_buffer_start;
+	}
+	private char[] yy_double (char buf[]) {
+		int i;
+		char newbuf[];
+		newbuf = new char[2*buf.length];
+		for (i = 0; i < buf.length; ++i) {
+			newbuf[i] = buf[i];
+		}
+		return newbuf;
+	}
+	private final int YY_E_INTERNAL = 0;
+	private final int YY_E_MATCH = 1;
+	private java.lang.String yy_error_string[] = {
+		"Error: Internal error.\n",
+		"Error: Unmatched input.\n"
+	};
+	private void yy_error (int code,boolean fatal) {
+		java.lang.System.out.print(yy_error_string[code]);
+		java.lang.System.out.flush();
+		if (fatal) {
+			throw new Error("Fatal Error.\n");
+		}
+	}
+	private int[][] unpackFromString(int size1, int size2, String st) {
+		int colonIndex = -1;
+		String lengthString;
+		int sequenceLength = 0;
+		int sequenceInteger = 0;
+
+		int commaIndex;
+		String workString;
+
+		int res[][] = new int[size1][size2];
+		for (int i= 0; i < size1; i++) {
+			for (int j= 0; j < size2; j++) {
+				if (sequenceLength != 0) {
+					res[i][j] = sequenceInteger;
+					sequenceLength--;
+					continue;
+				}
+				commaIndex = st.indexOf(',');
+				workString = (commaIndex==-1) ? st :
+					st.substring(0, commaIndex);
+				st = st.substring(commaIndex+1);
+				colonIndex = workString.indexOf(':');
+				if (colonIndex == -1) {
+					res[i][j]=Integer.parseInt(workString);
+					continue;
+				}
+				lengthString =
+					workString.substring(colonIndex+1);
+				sequenceLength=Integer.parseInt(lengthString);
+				workString=workString.substring(0,colonIndex);
+				sequenceInteger=Integer.parseInt(workString);
+				res[i][j] = sequenceInteger;
+				sequenceLength--;
+			}
+		}
+		return res;
+	}
+	private int yy_acpt[] = {
+		/* 0 */ YY_NOT_ACCEPT,
+		/* 1 */ YY_NO_ANCHOR,
+		/* 2 */ YY_NO_ANCHOR,
+		/* 3 */ YY_NO_ANCHOR,
+		/* 4 */ YY_NO_ANCHOR,
+		/* 5 */ YY_NO_ANCHOR,
+		/* 6 */ YY_NO_ANCHOR,
+		/* 7 */ YY_NO_ANCHOR,
+		/* 8 */ YY_NO_ANCHOR,
+		/* 9 */ YY_NO_ANCHOR,
+		/* 10 */ YY_NO_ANCHOR,
+		/* 11 */ YY_NO_ANCHOR,
+		/* 12 */ YY_NO_ANCHOR,
+		/* 13 */ YY_NO_ANCHOR,
+		/* 14 */ YY_NO_ANCHOR,
+		/* 15 */ YY_NO_ANCHOR,
+		/* 16 */ YY_NO_ANCHOR,
+		/* 17 */ YY_NO_ANCHOR,
+		/* 18 */ YY_NO_ANCHOR,
+		/* 19 */ YY_NO_ANCHOR,
+		/* 20 */ YY_NO_ANCHOR,
+		/* 21 */ YY_NO_ANCHOR,
+		/* 22 */ YY_NO_ANCHOR,
+		/* 23 */ YY_NO_ANCHOR,
+		/* 24 */ YY_NOT_ACCEPT,
+		/* 25 */ YY_NO_ANCHOR,
+		/* 26 */ YY_NOT_ACCEPT,
+		/* 27 */ YY_NO_ANCHOR,
+		/* 28 */ YY_NO_ANCHOR,
+		/* 29 */ YY_NO_ANCHOR,
+		/* 30 */ YY_NO_ANCHOR,
+		/* 31 */ YY_NO_ANCHOR,
+		/* 32 */ YY_NO_ANCHOR
+	};
+	private int yy_cmap[] = unpackFromString(1,130,
+"0:10,23,0:2,22,0:18,24,0:7,20,21,16,15,0,11,10,17,9:10,0:2,14,13,12,0:2,8:3" +
+",7,3,2,8:2,1,8:2,4,8,6,8:4,5,8:7,0:4,8,0,8:26,18,0,19,0:2,25:2")[0];
+
+	private int yy_rmap[] = unpackFromString(1,33,
+"0,1,2,3,4,5,6,5:10,7,5:3,8,7:2,9,7,8,10,11,12,13,14,15")[0];
+
+	private int yy_nxt[][] = unpackFromString(16,26,
+"-1,1,25,31,25:5,2,-1,3,4,5,6,7,8,9,10,11,12,13,24,14,15,16,-1,25,17,25:7,-1" +
+":25,2,26,-1:24,2,-1:29,18,-1:50,19,20,-1:13,25:9,-1:25,21,-1:39,14,-1:3,25:" +
+"2,22,25:6,-1:17,25,23,25:7,-1:17,25:4,27,25:4,-1:17,28,25:8,-1:17,25:3,29,2" +
+"5,32,25:3,-1:17,25:6,30,25:2,-1:16");
+
+	public java_cup.runtime.Symbol next_token ()
+		throws java.io.IOException {
+		int yy_lookahead;
+		int yy_anchor = YY_NO_ANCHOR;
+		int yy_state = yy_state_dtrans[yy_lexical_state];
+		int yy_next_state = YY_NO_STATE;
+		int yy_last_accept_state = YY_NO_STATE;
+		boolean yy_initial = true;
+		int yy_this_accept;
+
+		yy_mark_start();
+		yy_this_accept = yy_acpt[yy_state];
+		if (YY_NOT_ACCEPT != yy_this_accept) {
+			yy_last_accept_state = yy_state;
+			yy_mark_end();
+		}
+		while (true) {
+			if (yy_initial && yy_at_bol) yy_lookahead = YY_BOL;
+			else yy_lookahead = yy_advance();
+			yy_next_state = YY_F;
+			yy_next_state = yy_nxt[yy_rmap[yy_state]][yy_cmap[yy_lookahead]];
+			if (YY_EOF == yy_lookahead && true == yy_initial) {
+				return null;
+			}
+			if (YY_F != yy_next_state) {
+				yy_state = yy_next_state;
+				yy_initial = false;
+				yy_this_accept = yy_acpt[yy_state];
+				if (YY_NOT_ACCEPT != yy_this_accept) {
+					yy_last_accept_state = yy_state;
+					yy_mark_end();
+				}
+			}
+			else {
+				if (YY_NO_STATE == yy_last_accept_state) {
+					throw (new Error("Lexical Error: Unmatched Input."));
+				}
+				else {
+					yy_anchor = yy_acpt[yy_last_accept_state];
+					if (0 != (YY_END & yy_anchor)) {
+						yy_move_end();
+					}
+					yy_to_mark();
+					switch (yy_last_accept_state) {
+					case 1:
+						{ System.out.println("sym.IDE, yytext()"); }
+					case -2:
+						break;
+					case 2:
+						{ System.out.println("sym.INTCONST, new Integer(Integer.parseInt(yytext()))"); }
+					case -3:
+						break;
+					case 3:
+						{ System.out.println("sym.MINUS"); }
+					case -4:
+						break;
+					case 4:
+						{ System.out.println("sym.GT"); }
+					case -5:
+						break;
+					case 5:
+						{ System.out.println("sym.EQUAL"); }
+					case -6:
+						break;
+					case 6:
+						{ System.out.println("sym.LT"); }
+					case -7:
+						break;
+					case 7:
+						{ System.out.println("sym.ADD"); }
+					case -8:
+						break;
+					case 8:
+						{ System.out.println("sym.MULTI"); }
+					case -9:
+						break;
+					case 9:
+						{ System.out.println("sym.DIV"); }
+					case -10:
+						break;
+					case 10:
+						{ System.out.println("sym.LC"); }
+					case -11:
+						break;
+					case 11:
+						{ System.out.println("sym.RC"); }
+					case -12:
+						break;
+					case 12:
+						{ System.out.println("sym.LPAR"); }
+					case -13:
+						break;
+					case 13:
+						{ System.out.println("sym.RPAR"); }
+					case -14:
+						break;
+					case 14:
+						{ ++yyline; }
+					case -15:
+						break;
+					case 15:
+						{  }
+					case -16:
+						break;
+					case 16:
+						
+					case -17:
+						break;
+					case 17:
+						{ System.out.println("sym.IF"); }
+					case -18:
+						break;
+					case 18:
+						{ System.out.println("sym.GE"); }
+					case -19:
+						break;
+					case 19:
+						{ System.out.println("sym.UNEQ"); }
+					case -20:
+						break;
+					case 20:
+						{ System.out.println("sym.LE"); }
+					case -21:
+						break;
+					case 21:
+						{ System.out.println("sym.RATIONAL, new Double(Double.parseDouble(yytext()))"); }
+					case -22:
+						break;
+					case 22:
+						{ System.out.println("sym.ELSE"); }
+					case -23:
+						break;
+					case 23:
+						{ System.out.println("sym.ENDIF"); }
+					case -24:
+						break;
+					case 25:
+						{ System.out.println("sym.IDE, yytext()"); }
+					case -25:
+						break;
+					case 27:
+						{ System.out.println("sym.IDE, yytext()"); }
+					case -26:
+						break;
+					case 28:
+						{ System.out.println("sym.IDE, yytext()"); }
+					case -27:
+						break;
+					case 29:
+						{ System.out.println("sym.IDE, yytext()"); }
+					case -28:
+						break;
+					case 30:
+						{ System.out.println("sym.IDE, yytext()"); }
+					case -29:
+						break;
+					case 31:
+						{ System.out.println("sym.IDE, yytext()"); }
+					case -30:
+						break;
+					case 32:
+						{ System.out.println("sym.IDE, yytext()"); }
+					case -31:
+						break;
+					default:
+						yy_error(YY_E_INTERNAL,false);
+					case -1:
+					}
+					yy_initial = true;
+					yy_state = yy_state_dtrans[yy_lexical_state];
+					yy_next_state = YY_NO_STATE;
+					yy_last_accept_state = YY_NO_STATE;
+					yy_mark_start();
+					yy_this_accept = yy_acpt[yy_state];
+					if (YY_NOT_ACCEPT != yy_this_accept) {
+						yy_last_accept_state = yy_state;
+						yy_mark_end();
+					}
+				}
+			}
+		}
+	}
+}
